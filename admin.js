fc8e4018c78807557ce49d62c1b3e8cc93f9ed30
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2019-10-14 09:20:54 +0100

    connected sign up form to database

---
+++ admin.js
@@ -0,0 +1,31 @@
+function loginUser() {
+  $('.q').submit(function (e) {
+    e.preventDefault();
+    const email = $('#emailAddress').val();
+    const password = $('#password').val();
+    const url = $(this).attr('action');
+    if (email.length < 1) {
+      alert("Please enter your email address");
+    } else if (password.length < 1) {
+      alert("Please enter your password");
+    } else {
+      $.ajax({
+        url: url + `/?email=${email}&&password=${password}`,
+        type: 'GET',
+        dataType: 'json'
+      }).done((data) => {
+        console.log(data)
+        if (data.length === 0) {
+          alert('User login credentials incorrect')
+        } else {
+          if (data[0].isAdmin !== true) {
+            window.location.replace('../html/design.html')
+          } else {
+            window.location.replace('../html/create.html')
+          }
+        }
+      })
+    }
+    return false;
+  })
+}
