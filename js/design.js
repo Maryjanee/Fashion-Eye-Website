3e6564fd89957bbea52fdc40ff4980046535e1c9
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2019-10-18 15:41:23 +0100

    added search functionality

--- js/design.js
+++ js/design.js
@@ -6,18 +6,20 @@ $(document).ready(function () {
     success: function (data) {
       data.forEach((element, i) => {
         $('.container-content').append(
-          `
-              <div class="card content">
-                 <img src="../img/desgin_img/${element.designUrl}.jpg" class="card-img-top" alt="images" />
-                  <div class="card-body">
-                      <h5 class="card-title">${element.designName}</h5>
-                      <p class="card-text">${element.designDescription}</p>
-                      <p class="card-text">${element.designPrice}</p>
-                      <a href="#" class="btn btn-primary">Update</a>
-                      <a href="#" class="btn btn-primary">Delete</a>
+          `             
+                  <div class="card-group col-md-3 mt-3">
+                      <div class="card">
+                          <img src="../myimages/${element.designUrl}.jpg" class="card-img-top" alt="..." width="280px" height="220px">
+                          <div class="card-body">
+                          <h5 class="card-title">${element.designName}</h5>
+                          <p class="card-text">${element.designDescription}</p>
+                          </div>
+                          <div class="card-footer">
+                          <small class="text-muted">${element.designPrice}</small>
+                          </div>
+                      </div>
                   </div>
-           </div>
-           `
+                  `
         )
 
         $('tbody').append(`
@@ -29,8 +31,8 @@ $(document).ready(function () {
                   <td id="design_url">${element.designUrl}</td>
                   <td>
                       <div class="btn-group" role="group" aria-label="Basic example">
-                          <button type="button" class="btn btn-outline-light" id=${element.id} data-toggle="modal" data-target="#update-form-modal">Update</button>
-                          <button type="button" class="btn btn-outline-danger" id=${element.id}>Delete</button>
+                          <button type="button" class="btn btn-outline-light update-design-button" id=${element.id} data-toggle="modal" data-target="#update-form-modal">Update</button>
+                          <button type="button" class="btn btn-outline-danger delete-design" id=${element.id}>Delete</button>
                       </div>
                   </td>
               </tr>
@@ -40,6 +42,7 @@ $(document).ready(function () {
 
 
 
+      // search function
 
       $('#search-form').submit(function (e) {
         e.preventDefault();
@@ -51,29 +54,27 @@ $(document).ready(function () {
           dataType: "json",
           success: function (data) {
             $('tbody').replaceWith(`
-            <tr id=${data[0].id}>
-               <th scope="row">${1}</th>
-               <td id="design_name">${data[0].designName}</td>
-               <td id="design_desc">${data[0].designDescription}</td>
-               <td id="design_price">${data[0].designPrice}</td>
-               <td id="design_url">${data[0].designUrl}</td>
-               <td>
-                   <div class="btn-group" role="group" aria-label="Basic example">
-                       <button type="button" class="btn btn-outline-light" id=${data[0].id} data-toggle="modal" data-target="#update-form-modal">Update</button>
-                       <button type="button" class="btn btn-outline-danger" id=${data[0].id}>Delete</button>
-                   </div>
-               </td>
-           </tr>
-            `)
+                  <tr id=${data[0].id}>
+                     <th scope="row">${1}</th>
+                     <td id="design_name">${data[0].designName}</td>
+                     <td id="design_desc">${data[0].designDescription}</td>
+                     <td id="design_price">${data[0].designPrice}</td>
+                     <td id="design_url">${data[0].designUrl}</td>
+                     <td>
+                         <div class="btn-group" role="group" aria-label="Basic example">
+                             <button type="button" class="btn btn-outline-light" id=${data[0].id} data-toggle="modal" data-target="#update-form-modal">Update</button>
+                             <button type="button" class="btn btn-outline-danger" id=${data[0].id}>Delete</button>
+                         </div>
+                     </td>
+                 </tr>
+                  `)
 
           }
         })
       });
 
-
-
       // Delete button function
-      $('button.btn-outline-danger').on('click', function (e) {
+      $('button.delete-design').on('click', function (e) {
         $.ajax({
           url: `http://localhost:3000/designs/${e.target.id}`,
           type: "delete",
@@ -84,13 +85,13 @@ $(document).ready(function () {
       })
 
       //Update button function
-      $('button.btn-outline-light').on('click', function (e) {
+      $('button.update-design-button').on('click', function (e) {
         const id = e.target.id
 
-        $('#design-name').val($('#' + id).children('td[id=design_name]').text())
-        $('#design-price').val($('#' + id).children('td[id=design_price]').text())
-        $('#design-desc').val($('#' + id).children('td[id=design_desc]').text())
-        $('#design-url').val($('#' + id).children('td[id=design_url]').text())
+        $('#update-design-name').val($('#' + id).children('td[id=design_name]').text())
+        $('#update-design-price').val($('#' + id).children('td[id=design_price]').text())
+        $('#update-design-desc').val($('#' + id).children('td[id=design_desc]').text())
+        $('#update-design-url').val($('#' + id).children('td[id=design_url]').text())
 
 
         //window.localStorage.setItem("designupdate",JSON.stringify(designupdate_objs))
@@ -98,10 +99,10 @@ $(document).ready(function () {
 
         $('#update-changes').on('click', function () {
           const designupdate_objs = {
-            designName: $('#design-name').val(),
-            designPrice: $('#design-price').val(),
-            designDescription: $('#design-desc').val(),
-            designUrl: $('#design-url').val()
+            designName: $('#update-design-name').val(),
+            designPrice: $('#update-design-price').val(),
+            designDescription: $('#update-design-desc').val(),
+            designUrl: $('#update-design-url').val()
           }
 
           $.ajax({
@@ -116,12 +117,4 @@ $(document).ready(function () {
       })
     }
   });
-
-  // http://localhost:3000/designs?designUrl=m5
-
-
-
-
-
-
 });
